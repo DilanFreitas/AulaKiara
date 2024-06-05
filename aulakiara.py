52c7d3b841dae5a554cbf767f15ca8eb3754dd48
DilanFreitas <dilanfreitas10124@gmail.com>
2024-06-04 22:31:28 -0300

    primeiro commit

---
+++ aulakiara.py
@@ -0,0 +1,137 @@
+print("Programei esse joguinho pra gente jogar pra comemorar os nossos 10 meses")
+print("As regras são as seguintes, vão passar músicas que a gente escutou juntos e você terá que completar a letra")
+
+nome = input("Digite abaixo o nome do amor da minha vida: ")
+
+resp1 = input(f"Vamos começar? {nome} ")
+
+if resp1 == ("sim") or ("Sim") or ("Vamos"):
+    print("♫ Tocando: New West - Those Eyes")
+
+cause = input("Cause all of the small things that you do...")
+
+if cause == ("are what remind me why i fell for you"):
+    whenit = print(input("And when we're apart, and I'm missing you... "))
+
+    if whenit == ("i close my eyes and all i see is you") or ("i close my eyes and all i see is you"):
+      print("All the small things you do...")
+
+    else:
+      print("Errou ksks, mas tudo bem, pode tentar de novo <3")
+ 
+
+else:
+    print("Errou ksks, mas tudo bem, pode tentar de novo <3")
+   
+
+
+resp2 = input(f"Você se lembra? {nome}. ")
+
+if resp2 == ("Sim") or ("sim") or ("SIM"):
+    print("Escutei essa música pela primeira vez no ano de 2022 quando estavamos na minha varanda sozinhos em casa")
+print(f"Escutei essa música repetidas vezes no ano novo de 2023 enquanto eu não estava com você. {nome}.")
+print("Naquele dia, a sua ausência me deixou uma grande mágoa em uma data tão especial pra mim na época")
+print("Agora, nós passamos o final do ano de 2023 juntos, o que significa que nós evoluímos bastante daqui pra lá")
+
+print(input("Digite 1 para continuar: "))
+
+print("♫ Tocando: Jorge Ben Jor - Carolina Bela " )
+
+elamora = input("Eu falei, eu menti, eu chorei, eu sorri dizendo que ela mora no meu peito...")
+
+if elamora == ("e eu moro vizinho à ela") or ("e eu moro vizinho a ela"):
+    pensando = print(input("Que eu fico desse jeito... "))
+
+    if pensando == ("pensando nos beijos e nos carinhos dela") or ("pensando nos beijos e nos carinhos dela"):
+      print("Carolina, Carol, Carol, Carolina bela")
+
+    else:
+      print("Errou ksksksks mas pode tentar de novo")
+
+else:
+    print("Errou ksks, mas tudo bem, pode tentar de novo <3")
+
+
+
+resp3 = input(f"Você se lembra? {nome}. ")
+
+if resp3 == ("Sim") or ("sim") or ("SIM"):
+    print("A gente estava na piscina do seu prédio como sempre e assim percebemos que nós dois gostamos de mpb.")
+    print("Você me apresentou essa música e eu me apaixonei nela no mesmo momento em que você me mostrou ela")
+    print("Quando uma obra de arte remete ao algo que você sente ela se torna especial pra você")
+    print("E essa música me faz pensar que você mora no meu peito e eu moro vizinho a você")
+
+print(input("Digite (1) para continuar"))
+
+print("♫ Tocando: System of a down - Lonely Day")
+
+andif = input("And if you go... ")
+
+if andif == ("i wanna go with you"):
+    andifu = input("And if you die... ")
+
+else:
+    ("Errou ksksksks mas pode tentar de novo")
+
+
+if andifu == ("i wanna die with you"):
+    print("Você me disse que as músicas do System of a down eram na verdade músicas romanticas")
+    print("Apesar das vozes grossas e dos instrumentais frenéticos")
+    print("Essa música em específico também reflete aquilo que eu penso sobre você")
+    print("Afinal eu quero ir para aonde você for ou quer que eu vá <3")
+
+else:
+    print("Errou ksksksks mas pode tentar de novo")
+
+print(input("Digite (1) para continuar"))
+
+print("♫ Tocando: Konai x Kamaitachi - Perfume")
+
+seila = input("Nem precisa insistir que eu... ")
+
+if seila == ("Vou correndo pra ti") or ("vou correndo pra ti não") or ("vou correndo pra ti, não"):
+   olhe = print(input("Não tem mais ninguém que me olhe... "))
+
+   if olhe == ("como ela olha") or ("como ela olha"):
+    print("Não tem mais ninguém que me olhe")
+
+   else:
+    print("Errou ksksksks mas pode tentar de novo")
+
+else:
+    print("Errou ksksksks mas pode tentar de novo")
+
+
+
+
+resp3 = input(f"Você se lembra? {nome}. ")
+
+if resp3 == ("Sim") or ("sim") or ("SIM"):
+    print("O motivo pelo qual eu amo essa música é porque a forma em que você me olha é única")
+    print("Você foi o único que ao mesmo tempo em que me viu como quem eu realmente sou e gostou do que viu")
+    print("Você foi o único que viu valor em uma garota como eu")
+    print("Assim como a Haru em Beastars, agora eu não posso viver sem o meu lobo do bem que viu valor numa coelha como eu")
+
+print("******************************************************")
+            
+
+
+print("´´´¶¶¶¶¶´¶´´´´´´¶¶¶¶¶¶")
+print("´´¶¶¶¶¶¶¶¶¶¶´´¶¶¶¶¶¶¶¶¶¶")
+print("´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´¶¶¶¶")
+print("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´¶¶¶¶")
+print("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´¶¶¶¶¶")
+print("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ´¶¶¶¶¶´")
+print("´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶")
+print("´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶")
+print("´´´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶")
+print("´´´´´´´´´¶¶¶¶¶¶¶¶")
+print("´´´´´´´´´´´¶¶¶¶")
+
+print("Você chegou ao final do jogo e eu gostaria de dizer que eu queria ter feito algo mais bem feito")
+print("Mas nem mesmo se eu tivesse programado o próximo GTA inteiro e dedicado a você seria o suficiente")
+print("Pra retribuir tudo que você já fez por mim e pra demonstrar todo o amor que eu sinto por você")
+print(f"{nome}, Eu amo você do fundo do meu coração e fiz esse jogo para lembrar de 1% de tudo o que passamos juntos")
+print("e ainda iremos passar muito mais momentos juntos")
+print("    ")
+print("Assinado por Kiara Victória, para Dilan Martiniano Freitas")
